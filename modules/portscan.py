f5d1598855b0fcedb3f525eed77e34070e4eccf6
j3ssiejjj <j3ssiejjj@gmail.com>
2018-11-11 12:40:09 +0700

    fix same bug

--- modules/portscan.py
+++ modules/portscan.py
@@ -13,32 +13,35 @@ class PortScan(object):
 		self.aquaton()
 		self.eyewitness_common()
 		self.masscan()
-		# self.eyewitness_all()
+		self.eyewitness_all()
 
 	def aquaton(self):
 		utils.print_good('Starting aquatone')
 		cmd ='cat $WORKSPACE/subdomain/final-$TARGET.txt | $GO_PATH/aquatone -out $WORKSPACE/portscan/aquatone-common/$OUTPUT'
 		cmd = utils.replace_argument(self.options, cmd)
 		utils.print_info("Execute: {0} ".format(cmd))
+		execute.run(cmd)
+
 
 	def eyewitness_common(self):
 		utils.print_good('Starting EyeWitness for web')
-		cmd = 'python $PLUGINS_PATH/EyeWitness.py -f $WORKSPACE/subdomain/IP-$TARGET.txt --web --prepend-https --threads 20 -d $WORKSPACE/screenshot/'	
+		cmd = 'python $PLUGINS_PATH/EyeWitness/EyeWitness.py -f $WORKSPACE/subdomain/IP-$TARGET.txt --web --prepend-https --threads 20 -d $WORKSPACE/screenshot/'	
 		cmd = utils.replace_argument(self.options, cmd)
 		utils.print_info("Execute: {0} ".format(cmd))
+		execute.run(cmd)
 		print()
 
 	def masscan(self):
 		utils.print_good('Starting masscan')
 		cmd = 'sudo masscan -p0-65535 --rate 1000000 -iL $WORKSPACE/subdomain/final-$TARGET.txt -oG $WORKSPACE/portscan/$OUTPUT-masscan.gnmap -oX $WORKSPACE/portscan/$OUTPUT-masscan.xml --wait 0'
 		cmd = utils.replace_argument(self.options, cmd)
-		# execute.run_as_background(cmd)3
 		utils.print_info("Execute: {0} ".format(cmd))
+		execute.run(cmd)
 		print()
 
 	def eyewitness_all(self):
 		utils.print_good('Starting EyeWitness for all protocol')
-		cmd = 'python $PLUGINS_PATH/EyeWitness.py -x  $WORKSPACE/portscan/$OUTPUT-masscan.xml --web --all-protocols --prepend-https --threads 20 -d $WORKSPACE/screenshot/all/'	
+		cmd = 'python $PLUGINS_PATH/EyeWitness/EyeWitness.py -x  $WORKSPACE/portscan/$OUTPUT-masscan.xml --web --all-protocols --prepend-https --threads 20 -d $WORKSPACE/screenshot/all/'	
 		cmd = utils.replace_argument(self.options, cmd)
 		utils.print_info("Execute: {0} ".format(cmd))
 		print()
