7dbc5ce7fada87cbb2eb26375d69292d4b2fee94
j3ssiejjj <j3ssiejjj@gmail.com>
2019-01-22 10:41:26 +0700

    Fix typo

--- modules/ipspace.py
+++ modules/ipspace.py
@@ -3,20 +3,20 @@ from core import execute
 from core import utils
 
 class IPSpace(object):
-	''' Scanning vulnerable service based version '''
-	def __init__(self, options):
-		utils.print_banner("IP Discovery")
-		utils.make_directory(options['env']['WORKSPACE'] + '/ipspace/')
-		self.options = options
-		self.initial()
+    ''' Scanning vulnerable service based version '''
+    def __init__(self, options):
+        utils.print_banner("IP Discovery")
+        utils.make_directory(options['env']['WORKSPACE'] + '/ipspace/')
+        self.options = options
+        self.initial()
 
-	def initial(self):
-		ipOinst()
+    def initial(self):
+        self.ipOinst()
 
-	def ipOinst(self):
-		utils.print_good('Starting IPOinst')
-		cmd = '$PLUGINS_PATH/IPOsint/ip-osint.py -t $TARGET -o $WORKSPACE/ipspace/$OUTPUT-ipspace.txt'
-		cmd = utils.replace_argument(self.options, cmd)
-		utils.print_info("Execute: {0} ".format(cmd))
-		execute.run(cmd)
-		utils.check_output(self.options, '$WORKSPACE/ipspace/$OUTPUT-ipspace.txt')
+    def ipOinst(self):
+        utils.print_good('Starting IPOinst')
+        cmd = '$PLUGINS_PATH/IPOsint/ip-osint.py -t $TARGET -o $WORKSPACE/ipspace/$OUTPUT-ipspace.txt'
+        cmd = utils.replace_argument(self.options, cmd)
+        utils.print_info("Execute: {0} ".format(cmd))
+        execute.run(cmd)
+        utils.check_output(self.options, '$WORKSPACE/ipspace/$OUTPUT-ipspace.txt')
