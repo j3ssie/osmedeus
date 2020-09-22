fda2f6ac34eca432c315462995d2a5f180961983
j3ssie <jeromej3m@gmail.com>
2020-09-22 21:04:13 +0700

    Update Wappalyzer signatures

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ install_banner "providers-data for subdomain takeover"
 ####
 
 ## technology signature
-[[ -f $DATA_PATH/apps.json ]] || wget -q -O $DATA_PATH/apps.json https://raw.githubusercontent.com/AliasIO/Wappalyzer/master/src/apps.json
+[[ -f $DATA_PATH/apps.json ]] || wget -q -O $DATA_PATH/apps.json https://raw.githubusercontent.com/AliasIO/Wappalyzer/master/src/technologies.json
 
 ### Nmap stuff
 install_banner "nmap vulners nse"
@@ -138,15 +138,30 @@ fi
 PS="$ "
 source $DEFAULT_SHELL
 
+## Install latest go binary
+cd $CWD
 # update golang version
 install_banner "Install Golang latest version"
-wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash 2>&1 >/dev/null
+wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
+
 GO_BIN="$HOME/.go/bin/go"
 # in case the script fail
 [[ -f $GO_BIN ]] || GO_BIN=$(which go)
 echo -e "\033[1;32m[+] Detected go binary: $GO_BIN \033[0m"
 [[ -d $GO_DIR ]] || GO_DIR=$GOPATH/bin
 echo -e "\033[1;32m[+] Detected go tools: $GO_DIR \033[0m"
+
+GOROOT=$HOME/.go
+PATH=$GOROOT/bin:$PATH
+GOPATH=$HOME/go
+PATH=$GOPATH/bin:$PATH
+
+export GO_BIN="$HOME/.go/bin/go"
+export GOROOT=$HOME/.go
+export PATH=$GOROOT/bin:$PATH
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$PATH
+cd $CWD
 
 ##
 # Install go stuff
